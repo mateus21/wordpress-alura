691c743d3cb57ddeb9c544b98b1cd0b23c32a958
Mateus Mantovani <mateus@codebit.com.br>
2017-09-08 17:02:58 -0300

    finilizando single page post

--- wp-content/themes/malura/header.php
+++ wp-content/themes/malura/header.php
@@ -4,7 +4,7 @@
     <?php $home = get_template_directory_uri(); ?>
 
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="<?= $home ?>/reset.css">
+    <link rel="stylesheet" type="text/css" href="<?= $home ?>/assets/css/reset.css">
     <link rel="stylesheet" type="text/css" href="<?= $home; ?>/style.css">
     <?php wp_head(); ?>
 </head>
