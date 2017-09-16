970cf7718aa61e9d708065a30925dacda05515f1
Mateus Mantovani <mateus@codebit.com.br>
2017-09-16 20:33:30 -0300

    meta informacoes dos imoveis finalizada

--- wp-content/themes/malura/single.php
+++ wp-content/themes/malura/single.php
@@ -9,6 +9,7 @@ include_once ('header.php');
         if (have_posts()) {
             while (have_posts()) {
                 the_post();
+                $imoveis_meta_data = get_post_meta($post->ID);
                 ?>
                 <div class="single-imovel-thumbnail">
                     <?php the_post_thumbnail(); ?>
@@ -21,6 +22,17 @@ include_once ('header.php');
                         <div class="single-imovel-descricao">
                             <?php the_content(); ?>
                         </div>
+
+                        <dl class="single-imovel-informacoes">
+                            <dt>Preço:</dt>
+                            <dd><?= $imoveis_meta_data['preco_id'][0]; ?></dd>
+                            <dt>Vaga(s):</dt>
+                            <dd><?= $imoveis_meta_data['vagas_id'][0]; ?></dd>
+                            <dt>Banheiro(s):</dt>
+                            <dd><?= $imoveis_meta_data['banheiros_id'][0]; ?></dd>
+                            <dt>Quarto(s):</dt>
+                            <dd><?= $imoveis_meta_data['quartos_id'][0]; ?></dd>
+                        </dl>
                     </section>
 
                     <span class="single-imovel-data">
